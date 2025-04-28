4b1191ad5ff3f661f666a066cc068b65b9075e22
Mikolaj <mkolek@student.agh.edu.pl>
2025-04-28 08:42:17 +0200

    exc1 done

---
+++ zajecia_8/Program.cs
@@ -0,0 +1,107 @@
+﻿// 1. [3 punkty] Program szyfrujący przy pomocy kryptografii klucza asymetrycznego. Napisz program który jako parametr przyjmuje typ polecenia. W zależności od wybranego typu polecenia:
+//     - Jeżeli typ polecenia = 0 program ma wygenerować i zapisać do dwóch plików (o dowolnych nazwach, można je wpisać "na sztywno") klucz publiczny oraz klucz prywatny algorytmu RSA.
+//     - Jeżeli typ polecenia = 1, program dodatkowo pobiera nazwę dwóch plików (a), (b). Podany plik (a) ma zostać zaszyfrowany przy pomocy klucza publicznego odczytanego z pliku, który został stworzony przy pomocy tego programu kiedy typ polecenia = 0. Zaszyfrowane dane mają być zapisane w pliku (b).
+//     - Jeżeli typ polecenia = 2, program dodatkowo pobiera nazwę dwóch plików (a), (b). Podany plik (a) ma zostać odszyfrowany przy pomocy klucza prywatnego odczytanego z pliku, który został stworzony przy pomocy tego programu kiedy typ polecenia = 0. Odszyfrowane dane mają być zapisane w pliku (b).
+
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length < 1)
+        {
+            Console.WriteLine("Usage: Program <command> [<inputFile> <outputFile>]");
+            return;
+        }
+
+        int command = int.Parse(args[0]);
+
+        switch (command)
+        {
+            case 0:
+                GenerateKeys();
+                break;
+            case 1:
+                if (args.Length != 3)
+                {
+                    Console.WriteLine("Usage: Program 1 <inputFile> <outputFile>");
+                    return;
+                }
+                EncryptFile(args[1], args[2]);
+                break;
+            case 2:
+                if (args.Length != 3)
+                {
+                    Console.WriteLine("Usage: Program 2 <inputFile> <outputFile>");
+                    return;
+                }
+                DecryptFile(args[1], args[2]);
+                break;
+            default:
+                Console.WriteLine("Invalid command.");
+                break;
+        }
+    }
+    static void GenerateKeys()
+    {
+        using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(2048))
+        {
+            // Export public key
+            string publicKey = rsa.ToXmlString(false);
+            File.WriteAllText("publicKey.xml", publicKey);
+
+            // Export private key
+            string privateKey = rsa.ToXmlString(true);
+            File.WriteAllText("privateKey.xml", privateKey);
+
+            Console.WriteLine("Keys generated and saved to publicKey.xml and privateKey.xml.");
+        }
+    }
+    static void EncryptFile(string inputFile, string outputFile)
+    {
+        using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+        {
+            // Load public key
+            string publicKey = File.ReadAllText("publicKey.xml");
+            rsa.FromXmlString(publicKey);
+
+            // Read file data
+            byte[] dataToEncrypt = File.ReadAllBytes(inputFile);
+
+            // Encrypt data
+            byte[] encryptedData = rsa.Encrypt(dataToEncrypt, false);
+
+            // Write encrypted data to file
+            File.WriteAllBytes(outputFile, encryptedData);
+
+            Console.WriteLine($"File {inputFile} encrypted and saved to {outputFile}.");
+        }
+    }
+    static void DecryptFile(string inputFile, string outputFile)
+    {
+        using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+        {
+            // Load private key
+            string privateKey = File.ReadAllText("privateKey.xml");
+            rsa.FromXmlString(privateKey);
+
+            // Read encrypted data
+            byte[] encryptedData = File.ReadAllBytes(inputFile);
+
+            // Decrypt data
+            byte[] decryptedData = rsa.Decrypt(encryptedData, false);
+
+            // Write decrypted data to file
+            File.WriteAllBytes(outputFile, decryptedData);
+
+            Console.WriteLine($"File {inputFile} decrypted and saved to {outputFile}.");
+        }
+    }
+}
